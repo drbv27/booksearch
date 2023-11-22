d5ebf0cc3521936cd51789315b50eea522a81bd4
Diego Bonilla <drbv27@gmail.com>
2023-11-22 16:12:47 -0500

    fix the connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(/* process.env.MONGODB_URI */'mongodb+srv://drbv27:Sofia.0910@cluster0.1xobesi.mongodb.net/books' || 'mongodb://localhost/googlebooks', {
+mongoose.connect( process.env.MONGODB_URI  || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
